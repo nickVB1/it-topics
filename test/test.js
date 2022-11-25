d93d76f48ab7f8ce4aa7c3a74c27c0677bfbc2fc
nick van bergen <nick.vanbergen@hotmail.com>
2022-11-25 15:18:18 +0100

    aanpassing

--- test/test.js
+++ test/test.js
@@ -1,14 +1,27 @@
 const request = require('supertest');
-
-describe('get campus',()=>{
-    it('should return first campus', async ()=> {
-             await request('https://campus-api-example.netlify.app')
+const fs = require("fs")
+describe('get campus', () => {
+    it('should return first campus', async() => {
+        await request('https://campus-api-example.netlify.app')
             .get('/.netlify/functions/api/campus')
             .expect(200)
-            .expect('Content-Type','application/json; charset=utf-8')
-            .expect((res)=>{
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect((res) => {
+
+                fs.WriteFile("output.json", JSON.stringify(res.body[0]), 'utf8', function(err) {
+
+                    if (err) {
+                        console.log("An error occured while writing JSON object to file.");
+                        return console.log(err)
+                    }
+
+                    console.log("JSON file has been saved")
+                })
+
+
                 console.log(res.body[0])
             })
     });
+
 
 })
